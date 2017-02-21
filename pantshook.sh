526b1738807b5ee0341c02b658ecdb0f76c7211f
Sven De Gronckel <Sven@Svens-MacBook-Pro.local>
2017-02-21 08:31:14 +0100

    3th commit

--- pantshook.sh
+++ pantshook.sh
@@ -1,3 +1,3 @@
 date >> /home/sven/pantshook.log 2>&1
 cd /home/sven/mobileapp/
-git pull https://github.com/Hoegie/pantsapp.git
+git pull https://github.com/Hoegie/pantsapp.git >> /home/sven/pantshook.log 2>&1
